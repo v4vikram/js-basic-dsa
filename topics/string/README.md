658ccd4b7cb2d86eddeaadf0e2e9a6dbc3ce95a7
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-08-04 17:53:13 +0530

    Count vowels in string

--- topics/string/README.md
+++ topics/string/README.md
@@ -1,6 +1,16 @@
-# Reverse String in JavaScript
+# Reverse String
 
 - Start from the last charcter of the given string.
 - Loop backward untill the first charcter.
 - Append new charcter in new string.
 - Return the reverse string.
+
+# Count Vowels
+
+- Create an array of vowel characters (`a`, `e`, `i`, `o`, `u`).
+- Initialize a counter to keep track of vowel matches.
+- Loop through each vowel in the array.
+- For each vowel, iterate through all characters of the given string.
+- Compare the current vowel with each character in the string.
+- If a match is found, increment the counter.
+- Return the total count of vowels found in the string.
--- topics/string/string.js
+++ topics/string/string.js
@@ -1,9 +1,9 @@
-// 1. Iterate str 
-// 2. then do apposite (--) using decrement
-// 3. return
-
-
 function reverseString(str) {
+    // Start from the last charcter of the given string.
+    // Loop backward untill the first charcter.
+    // Append new charcter in new string.
+    // Return the reverse string.
+
 
     let reverseStr = "";
     for (let i = str.length - 1; i >= 0; i--) {
@@ -13,3 +13,26 @@ function reverseString(str) {
 
 }
 // console.log(reverseString("vikram"))
+
+function countVowels(str) {
+    // 1st put all vowels  in array
+    // 2nd  iterate all character of vowels
+    // 3nd iterate all character of str
+    // 4th matched all vowel char with string and increase count every time
+    // return it
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0;
+    for (let i = 0; i < vowels.length; i++) {
+
+        for (let v = 0; v <= str.length; v++) {
+
+            if (str[v] === vowels[i]) {
+                count += 1
+            }
+
+
+        }
+    }
+    return count
+}
+console.log(countVowels("vikramaee"))
